858cfb971ed0a77fa20581a76e4c0ce7ece6d514
Nihat-AYDIN <aydinmnihat2@gmail.com>
2024-03-02 14:47:16 +0300

    optional csv file name feature added.

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-const char *fname = "Food_contents_2019.csv";
 #define MAX_BUFFER_SIZE 3000
 #define MAX_SMALL_BUFFER_SIZE 50
+#define MAX_FILENAME_LENGTH 200
 
 int read_xline(FILE *stream, int lineno, char buffer[]);
 int read_xcol(const char *row, int colno, char buffer[]);
 
 int main(){
+    char buffer[MAX_BUFFER_SIZE];
+    char smallBuffer[MAX_SMALL_BUFFER_SIZE];
+    char fname[MAX_FILENAME_LENGTH];
 
     printf("Welcome to the CSV Parser application!\n");
     printf("This application takes a CSV file as input and returns the value at the specified row and column that you provide.\n\n");
+
+    printf("==> Please enter the name of the csv file: ");
+    scanf("%200s", fname);
 
     FILE *file = fopen(fname, "r");
 
@@ -28,9 +34,6 @@ int main(){
 
     printf("==> Please enter the column value: ");
     scanf("%d", &column);
-
-    char buffer[MAX_BUFFER_SIZE];
-    char smallBuffer[MAX_SMALL_BUFFER_SIZE];
 
     read_xline(file, row, buffer); 
     printf("\n\n==> All values in the requested row:\n");
